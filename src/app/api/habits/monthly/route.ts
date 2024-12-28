2b52b780e9b1f5af59cc5e216db862a7be9bb8fb
Matthieu Mary <83779030+Matthieu-Mary@users.noreply.github.com>
2024-12-28 16:29:59 +0100

    fix errors

--- src/app/api/habits/monthly/route.ts
+++ src/app/api/habits/monthly/route.ts
@@ -6,7 +6,6 @@ import { authOptions } from "../../../lib/auth";
 export async function GET() {
   try {
 
-    console.log("GET request received");
     const session = await getServerSession(authOptions);
 
     // Vérification de la session
@@ -47,40 +46,42 @@ export async function GET() {
 
     console.log(habits)
 
-    // const daysInMonth = lastDayOfMonth.getDate();
-    // const formattedHabits = habits.map(habit => {
-    //   const completionMap = new Map(
-    //     habit.completions.map(completion => [
-    //       completion.date.toISOString().split('T')[0],
-    //       completion.completed
-    //     ])
-    //   );
+    const daysInMonth = lastDayOfMonth.getDate();
+    const formattedHabits = habits.map(habit => {
+      const completionMap = new Map(
+        habit.completions.map(completion => [
+          completion.date.toISOString().split('T')[0],
+          completion.completed
+        ])
+      );
 
-    //   const dailyStatus = Array.from({ length: daysInMonth }, (_, index) => {
-    //     const date = new Date(now.getFullYear(), now.getMonth(), index + 1);
-    //     const dateStr = date.toISOString().split('T')[0];
+      const dailyStatus = Array.from({ length: daysInMonth }, (_, index) => {
+        const date = new Date(now.getFullYear(), now.getMonth(), index + 1);
+        const dateStr = date.toISOString().split('T')[0];
         
-    //     return {
-    //       date: dateStr,
-    //       day: index + 1,
-    //       completed: completionMap.get(dateStr) ?? false
-    //     };
-    //   });
+        return {
+          date: dateStr,
+          day: index + 1,
+          completed: completionMap.get(dateStr) ?? false
+        };
+      });
 
-    //   return {
-    //     id: habit.id,
-    //     title: habit.title,
-    //     description: habit.description,
-    //     dailyStatus,
-    //   };
-    // });
+      return {
+        id: habit.id,
+        title: habit.name,
+        description: habit.description,
+        dailyStatus,
+      };
+    });
 
-    // return NextResponse.json({
-    //   month: now.getMonth() + 1,
-    //   year: now.getFullYear(),
-    //   daysInMonth,
-    //   habits: formattedHabits
-    // });
+    console.log(formattedHabits)
+
+    return NextResponse.json({
+      month: now.getMonth() + 1,
+      year: now.getFullYear(),
+      daysInMonth,
+      habits: formattedHabits
+    });
 
     return NextResponse.json({ habits });
   } catch (error) {
