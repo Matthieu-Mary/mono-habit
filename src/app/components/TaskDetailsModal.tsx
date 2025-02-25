378fa45f8cfe48bcc72f000b886b6167c93c7e8d
Matthieu Mary <83779030+Matthieu-Mary@users.noreply.github.com>
2025-02-25 19:43:30 +0100

    Suppression de la méthode getTaskTypeColor  dans le ocmpoant pour utiliser celle dans le fichier utils

--- src/app/components/TaskDetailsModal.tsx
+++ src/app/components/TaskDetailsModal.tsx
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useState, useEffect } from "react";
 import { Status, TaskType } from "../types/enums";
 import { TaskModalProps } from "./TaskModal";
+import { getTaskTypeColor } from "../utils/taskTypeUtils";
 
 interface TaskDetailsModalProps extends TaskModalProps {
   task: {
@@ -15,40 +16,6 @@ interface TaskDetailsModalProps extends TaskModalProps {
   } | null;
   isFutureDate: boolean;
 }
-
-const getTaskTypeColor = (type: TaskType) => {
-  const colorMap = {
-    [TaskType.SPORT]: {
-      bg: "bg-orange-100",
-      text: "text-orange-600",
-      border: "border-orange-200",
-      hover: "hover:bg-orange-200",
-      icon: "🏃‍♂️",
-    },
-    [TaskType.SANTE]: {
-      bg: "bg-emerald-100",
-      text: "text-emerald-600",
-      border: "border-emerald-200",
-      hover: "hover:bg-emerald-200",
-      icon: "💚",
-    },
-    [TaskType.TRAVAIL]: {
-      bg: "bg-blue-100",
-      text: "text-blue-600",
-      border: "border-blue-200",
-      hover: "hover:bg-blue-200",
-      icon: "💼",
-    },
-    [TaskType.LOISIRS]: {
-      bg: "bg-purple-100",
-      text: "text-purple-600",
-      border: "border-purple-200",
-      hover: "hover:bg-purple-200",
-      icon: "🎮",
-    },
-  };
-  return colorMap[type];
-};
 
 export default function TaskDetailsModal({
   isOpen,
